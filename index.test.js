0a684b7e7c71f0de44b6b463c48e7f0f7519e9b5
Andrew Borovin <a@borovin.com>
2016-06-10 01:50:41 +0300

    increase code coverage

--- index.test.js
+++ index.test.js
@@ -13,3 +13,32 @@ test('create base class', t => {
   t.is(baseClass.a, 1);
 });
 
+test('init without new', t => {
+  const baseClass = createClass({
+    constructor() {
+      this.a = 1;
+    },
+  });
+
+  const instance = baseClass();
+
+  t.is(instance.a, 1);
+});
+
+test('extend class', t => {
+  const BaseClass = createClass({
+    constructor() {
+      this.a = 1;
+    },
+  });
+
+  const ChildClass = BaseClass.extend({
+    b: 2,
+  });
+
+  const childClass = new ChildClass();
+
+  t.is(childClass.a, 1);
+  t.is(childClass.b, 2);
+});
+
